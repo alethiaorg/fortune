8d25668d256a27baae2a79787437cf05c8e0cd98
Angelo Carasig <angelocarasig15@gmail.com>
2025-05-11 01:01:30 +1000

    uri encode for rising

--- src/routes/mangadex/endpoints/get-rising.ts
+++ src/routes/mangadex/endpoints/get-rising.ts
@@ -1,10 +1,9 @@
+import { encodeUri } from '@/util';
 import customBuilder from '../helpers/custom-builder';
 
 const endpoint = customBuilder({
-	order: { followedCount: 'desc' },
-	createdAtSince: new Date(new Date().setMonth(new Date().getMonth() - 1))
-		.toISOString()
-		.slice(0, 19)
+  order: { followedCount: 'desc' },
+  createdAtSince: encodeUri(new Date(new Date().setMonth(new Date().getMonth() - 1)).toISOString().slice(0, 19))
 });
 
 export default endpoint;
